ab8554442d288b26d77fd237647d017a8142704f
toosynxed <bconomy.wiki@gmail.com>
2025-10-18 10:22:27 +1100

    Update bible.service.ts

--- src/app/bible.service.ts
+++ src/app/bible.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable, map } from 'rxjs';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 import { Book, Verse } from './bible';
 
 @Injectable({

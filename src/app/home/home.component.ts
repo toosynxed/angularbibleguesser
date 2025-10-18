9af6514485fae7a32c62836e963588dbea796d3c
toosynxed <bconomy.wiki@gmail.com>
2025-10-19 09:37:55 +1100

    Implement ChangeLog

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -11,13 +11,29 @@ export class HomeComponent {
   showCodeInput = false;
   gameCode = '';
   error: string | null = null;
+  showChangelog = false;
+
+  // You can update the changelog text here.
+  // Using backticks (`) allows for multi-line strings.
+  changelogContent = `
+### Version 1.2.0
+- Added a changelog to keep you updated!
+
+### Version 1.1.0
+- Integrated all verses into a single paragraph on the game screen.
+- The verse to be guessed is now highlighted in purple.
+- Increased the difficulty slider range to 250 verses.
+- Added a "Refocus" button to scroll back to the verse being guessed.
+
+### Version 1.0.0
+- Initial release of Better Bible Guesser.
+  `;
 
   constructor(private router: Router, private shareService: ShareService) { }
 
   startGame(mode: 'normal' | 'marathon'): void {
     this.router.navigate(['/game'], { state: { mode: mode } });
   }
-
   playFromCode(): void {
     this.error = null;
     const seed = this.shareService.decodeGame(this.gameCode);

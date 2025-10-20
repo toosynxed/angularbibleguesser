cb2ef3738f931ce2d86dbf9f13fdfb40047acad5
toosynxed <bconomy.wiki@gmail.com>
2025-10-20 14:56:20 +1100

    step

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,11 +1,10 @@
+// This is a new file: src/environments/environment.ts
 export const environment = {
-  production: true,
-  firebase: {
-    apiKey: "AIzaSyAvyDN6dR8CI6mZskn7ey8rtjQMj3UzmNE",
-    authDomain: "angularbetterbibleguesser.firebaseapp.com",
-    projectId: "angularbetterbibleguesser",
-    storageBucket: "angularbetterbibleguesser.firebasestorage.app",
-    messagingSenderId: "108645434073",
-    appId: "1:108645434073:web:a9c518c0ba7323321674c8"
-  }
+  apiKey: "AIzaSyAvyDN6dR8CI6mZskn7ey8rtjQMj3UzmNE",
+  authDomain: "angularbetterbibleguesser.firebaseapp.com",
+  projectId: "angularbetterbibleguesser",
+  storageBucket: "angularbetterbibleguesser.firebasestorage.app",
+  messagingSenderId: "108645434073",
+  appId: "1:108645434073:web:a9c518c0ba7323321674c8",
+  measurementId: "G-GGNBX88D62"
 };
